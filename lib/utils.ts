1ba766e795770553d6e0a1756ba2aafd29389556
mk <mk.eng1119@gmail.com>
2024-03-12 06:22:51 +0900

    add validate function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { ZodSchema, z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function validate<T extends ZodSchema>(target: unknown, schema: T): asserts target is z.infer<T> {
+  schema.parse(target);
 }
