47c623c45dcb5a3f75b5c4c0a3078e32f981210d
mk <mk.eng1119@gmail.com>
2024-03-15 19:42:32 +0900

    delete

--- app/schemas/auth.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const authFormSchema = z.object({
-  email: z.string().email().min(1),
-  name: z.string().min(1, 'Name is required'),
-});
--- app/types/auth.ts
+++
@@ -1,4 +0,0 @@
-import { z } from 'zod';
-import { authFormSchema } from '@/app/schemas/auth';
-
-export type AuthForm = z.infer<typeof authFormSchema>;
